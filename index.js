995009b34cb9838bb3a0b6df9f78a68a479b64c8
Don Han <don_han@berkeley.edu>
2015-03-28 20:58:11 -0700

    html/css works

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
